0c389cff4130664f13226a6996e7e62743a6b61e
CJBJ1 <46565543+CJBJ1@users.noreply.github.com>
2019-05-02 16:43:29 +0900

    삭제버튼 추가

--- js/newPoint.js
+++ js/newPoint.js
@@ -5,34 +5,56 @@ function newPoint(map,markerList){
             map: map,
             clickable: true,
             name: markerList.length,
+            title: '새 출발지',
             visible : true
         });
         naver.maps.Event.addListener(marker, "click", function(e) {
             delPointByMarker(marker);
         });
-        
         markerList.push(marker);
         appendBymarker(markerList);
-        
+        delPoint(markerList);
+        changeName();
     });
 }
+
 function delPoint(markerList){
-    var i;
-    $('.nav-item').click(function(){
-        i=$(this).attr('name');
-        markerList[i].setMap(null);
-        markerList[i].visible=false;
-        $(this).remove();
+    var order;
+    $('.delbtn').click(function(){
+        order=$(this).attr('name');
+        markerList[order].setMap(null);
+        markerList[order].visible=false;
+        $(document.getElementsByName(order)).remove();
     });
+
 }
+
 function appendBymarker(markerList){
-    let text = `<li name ='${markerList.length-1}' class='nav-item'><a class='nav-link' onclick="delPoint(markerList)"><span data-feather='file-text'></span>새 출발지</a></li>`;
+    let text = `<li name ='${markerList.length-1}' class='nav-item'>
+    <a class='nav-link'>
+    <span data-feather='file-text'></span>새 출발지
+    <button type="button" class='changebtn' name='${markerList.length-1}' style="height:auto;width:auto"
+    onclick="">변경
+    </button>
+    <button type="button" class='delbtn' name='${markerList.length-1}' style="height:auto;width:auto"
+    onclick="">삭제
+    </button></a></li>`
     $('#addedPlace').append(text);
 }
+
 function delPointByMarker(marker){
     marker.setMap(null);
     marker.visible=false;
     $(document.getElementsByName(marker.name)).remove();
+}
+
+function changeName(){
+    var order;
+    $('.changebtn').click(function(){
+        var par = $(this).parents('a');
+        var cloneEle = par.children();
+        par.text('이름 변경됨');
+    });
 }
 
 function eMarkerList(markerList){
